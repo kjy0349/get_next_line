9c40c0b5a367f6b6b38d151dbe21579f49b6bd8e
kjy0349 <kjy0349@naver.com>
2022-06-02 14:44:54 +0900

    modified

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jeykim <jeykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 14:50:22 by jeykim            #+#    #+#             */
-/*   Updated: 2022/05/24 20:08:25 by jeykim           ###   ########.fr       */
+/*   Updated: 2022/06/02 14:43:40 by jeykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,32 @@ char	*join_and_getline(t_blist **buff_list, char *buffer, int fd)
 	if (*buff_list)
 		ft_strjoin((*buff_list)->buffer, buffer);
 	else
+	{
+		*buff_list = (t_blist *)malloc(sizeof(t_blist));
+		(*buff_list)->fd = fd;
 		(*buff_list)->buffer = buffer;
+		(*buff_list)->next = NULL;
+	}
 	return ((*buff_list)->buffer);
 }
 
 char	*get_line(char	*buffer)
 {
-	int	size;
-	int	i;
+	int		size;
+	int		i;
+	char	*line;
 
 	size = 0;
 	while (buffer[size] != '\n' || buffer[size] != -1)
 		size++;
+	line = (char *)malloc(sizeof(char) * size);
 	i = 0;
 	while (i < size)
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	return (line);
 }
 
 char	*get_next_line(int fd)
@@ -49,7 +61,8 @@ char	*get_next_line(int fd)
 	return_num = read(fd, buffer, BUFFER_SIZE);
 	if (fd < 0 || BUFFER_SIZE <= 0 || return_num == -1)
 		return (NULL);
-	get_line(join_and_getline(&buff_list, buffer, fd));
+	ret_line = get_line(join_and_getline(&buff_list, buffer, fd));
+	return (ret_line);
 }
 
 #include <stdio.h>
